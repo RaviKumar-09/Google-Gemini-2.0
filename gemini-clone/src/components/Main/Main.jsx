68c73ef8dd0327f2e22c2750259268530932d738
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-23 10:42:12 +0530

    create cards container

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 //import React from 'react'
 import './Main.css'
-import {assets} from '../../assets/assets'
+import { assets } from '../../assets/assets'
 const Main = () => {
     return (
         <div className='main'>
@@ -13,8 +13,26 @@ const Main = () => {
                     <p><span>Hello,Ravi.</span></p>
                     <p>How can I help you today?</p>
                 </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful palces to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstrom team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
             </div>
-        
+
         </div>
     )
 }
