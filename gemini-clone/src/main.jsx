3f2cb71914fa52366c5c5150832a994e57dde926
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-24 22:07:04 +0530

    rap contextProvider in mani.jsx

--- gemini-clone/src/main.jsx
+++ gemini-clone/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//import { StrictMode } from 'react'
+import ReactDom from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ContextProvider from './context/Context.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDom.createRoot(document.getElementById('root')).render(
+  <ContextProvider>
     <App />
-  </StrictMode>,
+  </ContextProvider>,
 )
