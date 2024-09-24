054231e8d3ea08e745ba8d4447fac0d591351894
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-24 22:12:45 +0530

    import generative-ai package

--- gemini-clone/src/config/gemini.js
+++ gemini-clone/src/config/gemini.js
@@ -5,11 +5,12 @@
  * $ npm install @google/generative-ai
  */
 
-const {
+import {
     GoogleGenerativeAI,
     HarmCategory,
     HarmBlockThreshold,
-  } = require("@google/generative-ai");
+    
+  } from "@google/generative-ai";
   
   const apiKey = "AIzaSyAP0kELjYHsn89a8o4MBVy4-tKm4LlupDY";
   const genAI = new GoogleGenerativeAI(apiKey);
