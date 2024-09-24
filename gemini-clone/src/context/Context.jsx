fea93cbfab704cca849b0281c6417dc5c19d2cdf
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-24 22:11:04 +0530

    creat async funtion onSent

--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -1,10 +1,17 @@
 import { createContext } from "react";
+import run from "../config/gemini";
 
 
 export const Context = createContext();
 
 const ContextProvider = (props) =>{
 
+
+    const onSent = async (prompt) => {
+        await run(prompt)
+    }
+
+    onSent("What is react js")
 
     const contextValue = {
 
